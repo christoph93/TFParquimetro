717d65335e563a1b6a92ed012fb874241280cf99
ccalifi <ccalifi@PC000BR21441.ad.esi.adp.com>
2015-11-26 18:25:25 -0200

    Criação inicial da GUI

---
+++ src/negocio/Facade.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio;
+
+/**
+ *
+ * @author ccalifi
+ */
+public class Facade {
+    
+    
+    public void cancela(){
+        
+    }
+    
+    public void imprime(){
+        
+    }
+    
+    public boolean decrementaTempo(){
+        return false;
+    }
+    
+    public boolean incrementaTempo(){
+        return false;
+    }
+    
+    public boolean insereMoeda(String moeda){
+        return false;
+    }
+    
+    public boolean pagaComCartao(){
+        return false;
+    }
+    
+    
+}
